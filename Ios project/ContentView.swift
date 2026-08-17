e4935f7ff581d1e28aaaa38e41e7418d6ed6c971
yoonfreal <u6642005@au.edu>
2026-08-18 03:11:14 +0700

    Initial Commit

---
+++ Ios project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ios project
+//
+//  Created by Yoon Hsu Hlaing on 18/8/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ios project/Ios_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_projectApp.swift
+//  Ios project
+//
+//  Created by Yoon Hsu Hlaing on 18/8/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct Ios_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
